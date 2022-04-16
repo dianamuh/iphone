2b6c5bf9325603865d486537815abce8f1e11240
dianamuh <92950252+dianamuh@users.noreply.github.com>
2022-04-17 00:07:09 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let yMob= document.getElementById('yellowmob');
+let main=document.querySelector('.main')
+let btn=document.querySelector('.image')
+function changeMob(mobSrc){
+    yMob.src=mobSrc;
+}
+function changeColor(color){
+    main.style.background=color;
+}
