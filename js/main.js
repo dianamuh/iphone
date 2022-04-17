e41ddf63a1c7f0ba97dc5c7a244fa6cc45143522
dianamuh <92950252+dianamuh@users.noreply.github.com>
2022-04-18 00:02:54 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 let yMob= document.getElementById('yellowmob');
 let main=document.querySelector('.main')
 let btn=document.querySelector('.image')
+let nav =document.querySelector(".navbar")
 function changeMob(mobSrc){
     yMob.src=mobSrc;
 }
 function changeColor(color){
     main.style.background=color;
+    nav.style.background=color
 }
